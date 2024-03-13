2a26c982829446dbba7276e1631cbb85ad6beb3c
Moises Almeida <moisesalmeida.dev@gmail.com>
2024-03-12 20:10:03 -0400

    Criando enum para tipo Cliente

---
+++ src/main/java/tech/ada/rental/enums/TipoCliente.java
@@ -0,0 +1,7 @@
+package tech.ada.rental.enums;
+
+public enum TipoCliente {
+
+    PESSOA_FISICA,
+    PESSOA_JURIDICA
+}
--- src/main/java/tech/ada/rental/model/Cliente.java
+++ src/main/java/tech/ada/rental/model/Cliente.java
@@ -1,5 +1,7 @@
 package tech.ada.rental.model;
 
+import tech.ada.rental.enums.TipoCliente;
+
 import java.util.Objects;
 
 public class Cliente {
@@ -8,15 +10,17 @@ public class Cliente {
     private String nome;
     private String email;
     private String telefone;
-    private String cpf;
+    private String documento;
     private String cnh;
+    private TipoCliente tipo;
 
-    public Cliente(String nome, String email, String telefone, String cpf, String cnh) {
+    public Cliente(String nome, String email, String telefone, String documento, String cnh, TipoCliente tipo) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
-        this.cpf = cpf;
+        this.documento = documento;
         this.cnh = cnh;
+        this.tipo = tipo;
     }
 
     public Long getId() {
@@ -47,12 +51,12 @@ public class Cliente {
         this.telefone = telefone;
     }
 
-    public String getCpf() {
-        return cpf;
+    public String getDocumento() {
+        return documento;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public void setDocumento(String documento) {
+        this.documento = documento;
     }
 
     public String getCnh() {
@@ -63,16 +67,24 @@ public class Cliente {
         this.cnh = cnh;
     }
 
+    public TipoCliente getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Cliente cliente = (Cliente) o;
-        return Objects.equals(id, cliente.id) && Objects.equals(nome, cliente.nome) && Objects.equals(email, cliente.email) && Objects.equals(telefone, cliente.telefone) && Objects.equals(cpf, cliente.cpf) && Objects.equals(cnh, cliente.cnh);
+        return Objects.equals(id, cliente.id) && Objects.equals(nome, cliente.nome) && Objects.equals(email, cliente.email) && Objects.equals(telefone, cliente.telefone) && Objects.equals(documento, cliente.documento) && Objects.equals(cnh, cliente.cnh);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nome, email, telefone, cpf, cnh);
+        return Objects.hash(id, nome, email, telefone, documento, cnh);
     }
 }
