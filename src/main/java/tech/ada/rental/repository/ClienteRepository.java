eb35ac10a9ff6be3b0788d711131ba2f442b5dac
Moises Almeida <moisesalmeida.dev@gmail.com>
2024-03-13 21:00:45 -0400

    Criando serviço de salvar Clinte

--- src/main/java/tech/ada/rental/repository/ClienteRepository.java
+++ src/main/java/tech/ada/rental/repository/ClienteRepository.java
@@ -3,4 +3,6 @@ package tech.ada.rental.repository;
 import tech.ada.rental.model.Cliente;
 
 public interface ClienteRepository extends Repository<Cliente> {
+
+    Cliente findByDocumento(String documento);
 }
