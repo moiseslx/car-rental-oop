e5665b4c78b35c27d6c9df2a246355a47f528b07
cinbrito <cin.brito@outlook.com>
2024-03-15 15:44:31 -0300

    criando classe de validação

--- src/main/java/tech/ada/rental/controller/ClienteController.java
+++ src/main/java/tech/ada/rental/controller/ClienteController.java
@@ -4,6 +4,7 @@ import tech.ada.rental.dto.ClienteDTO;
 import tech.ada.rental.model.Cliente;
 import tech.ada.rental.service.ClienteService;
 import tech.ada.rental.service.exception.ElementosDuplicadosException;
+import tech.ada.rental.controller.validators.UserEntryValidator;
 
 public class ClienteController {
     ClienteService clienteService;
@@ -14,7 +15,9 @@ public class ClienteController {
 
     public Cliente criar(ClienteDTO clienteDTO) throws ElementosDuplicadosException {
         // TODO validar entrada de dados
-
+        boolean documentoValido = UserEntryValidator.validateDocumento(clienteDTO.documento());
+        boolean nomeValido = UserEntryValidator.validateNome(clienteDTO.nome());
+        boolean cnhValida = UserEntryValidator.validarCNH(clienteDTO.cnh());
         return clienteService.criar(clienteDTO.toCliente());
     }
 }
