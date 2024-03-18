009eb3aad10f0e22ed9bdbf55c75d4e709af5cc1
cinbrito <cin.brito@outlook.com>
2024-03-18 16:09:22 -0300

    implementando aluguel service

--- src/main/java/tech/ada/rental/dto/AluguelDTO.java
+++ src/main/java/tech/ada/rental/dto/AluguelDTO.java
@@ -1,9 +1,14 @@
 package tech.ada.rental.dto;
 
+import tech.ada.rental.model.Aluguel;
 import tech.ada.rental.model.Cliente;
 import tech.ada.rental.model.Veiculo;
 
 import java.time.LocalDateTime;
 
 public record AluguelDTO(Cliente cliente, Veiculo veiculo, LocalDateTime inicioAluguel) {
+
+    public Aluguel toAluguel() {
+        return new Aluguel(cliente, veiculo, inicioAluguel);
+    }
 }
